9e4e5ae0e6fb7dcf88507c085b9352c450624fca
Jørgen Lind <jorgen@myvr-software.com>
2016-01-16 08:40:59 +0100

    Implemented hash and not only hashEncoded

--- src/Crypto/Argon2.hs
+++ src/Crypto/Argon2.hs
@@ -34,22 +34,22 @@ hashEncoded :: HashOptions -- ^ Options pertaining to how expensive the hash is 
             -> BS.ByteString -- ^ The salt to use when hashing
             -> T.Text -- ^ The encoded password hash
 hashEncoded options password salt =
-  unsafePerformIO (hash' options password salt FFI.argon2i_hash_encoded FFI.argon2d_hash_encoded asText)
-  where asText = fmap T.decodeUtf8 . BS.packCString
+  unsafePerformIO (hashEncoded' options password salt FFI.argon2i_hash_encoded FFI.argon2d_hash_encoded)
 
 hash :: HashOptions -- ^ Options pertaining to how expensive the hash is to calculate
      -> BS.ByteString -- ^ The password to hash
      -> BS.ByteString -- ^ The salt to use when hashing
      -> BS.ByteString -- ^ The un-encoded password hash
 hash options password salt =
-  unsafePerformIO (hash' options password salt FFI.argon2i_hash_encoded FFI.argon2d_hash_encoded BS.packCString)
+  unsafePerformIO (hash' options password salt FFI.argon2i_hash_raw FFI.argon2d_hash_raw)
 
 variant :: a -> a -> Argon2Variant -> a
 variant a _ Argon2i = a
 variant _ b Argon2d = b
 {-# INLINE variant #-}
 
-type Argon2 a = Word32 -> Word32 -> Word32 -> CString -> Word64 -> CString -> Word64 -> Word64 -> a -> Word64 -> IO Int32
+type Argon2Encode = Word32 -> Word32 -> Word32 -> CString -> Word64 -> CString -> Word64 -> Word64 -> CString -> Word64 -> IO Int32
+type Argon2Raw    = Word32 -> Word32 -> Word32 -> CString -> Word64 -> CString -> Word64 -> CString -> Word64 -> IO Int32
 
 data Argon2Exception
   = Argon2PasswordLengthOutOfRange Word64 Word64 Word64
@@ -62,17 +62,49 @@ data Argon2Exception
 
 instance Exception Argon2Exception
 
-hash' :: HashOptions
-      -> BS.ByteString
-      -> BS.ByteString
-      -> Argon2 (Ptr a)
-      -> Argon2 (Ptr a)
-      -> (Ptr a -> IO b)
-      -> IO b
-hash' HashOptions{..} password salt argon2i argon2d postProcess =
+handleSuccessCode :: Int32
+                  -> CString
+                  -> Word64
+                  -> Word64
+                  -> Word32
+                  -> Word32
+                  -> Word32
+                  -> IO BS.ByteString
+handleSuccessCode res out saltLen passwordLen hashIterations hashMemory hashParallelism =
+  case res of
+    a
+      | a `elem` [FFI.ARGON2_OK] -> BS.packCString out
+      | a `elem` [FFI.ARGON2_SALT_TOO_SHORT,FFI.ARGON2_SALT_TOO_LONG] ->
+        throwIO (Argon2SaltLengthOutOfRange saltLen
+                                            FFI.ARGON2_MIN_SALT_LENGTH
+                                            FFI.ARGON2_MAX_SALT_LENGTH)
+      | a `elem` [FFI.ARGON2_PWD_TOO_SHORT,FFI.ARGON2_PWD_TOO_LONG] ->
+        throwIO (Argon2PasswordLengthOutOfRange passwordLen
+                                                FFI.ARGON2_MIN_PWD_LENGTH
+                                                FFI.ARGON2_MAX_PWD_LENGTH)
+      | a `elem` [FFI.ARGON2_TIME_TOO_SMALL,FFI.ARGON2_TIME_TOO_LARGE] ->
+        throwIO (Argon2IterationCountOutOfRange hashIterations
+                                                FFI.ARGON2_MIN_TIME
+                                                FFI.ARGON2_MAX_TIME)
+      | a `elem` [FFI.ARGON2_MEMORY_TOO_LITTLE,FFI.ARGON2_MEMORY_TOO_MUCH] ->
+        throwIO (Argon2MemoryUseOutOfRange
+                   hashMemory
+                   (max FFI.ARGON2_MIN_MEMORY (8 * hashParallelism))
+                   FFI.ARGON2_MAX_MEMORY)
+      | a `elem` [FFI.ARGON2_LANES_TOO_FEW,FFI.ARGON2_LANES_TOO_MANY] ->
+        throwIO (Argon2ParallelismOutOfRange hashParallelism
+                                             FFI.ARGON2_MIN_LANES
+                                             FFI.ARGON2_MAX_LANES)
+      | otherwise -> throwIO (Argon2Exception a)
+
+hashEncoded' :: HashOptions
+             -> BS.ByteString
+             -> BS.ByteString
+             -> Argon2Encode
+             -> Argon2Encode
+             -> IO T.Text
+hashEncoded' HashOptions{..} password salt argon2i argon2d =
   do out <- mallocBytes 512
-     let saltLen = fromIntegral (BS.length salt)
-         passwordLen = fromIntegral (BS.length password)
      res <-
        BS.useAsCString password $
        \password' ->
@@ -88,33 +120,39 @@ hash' HashOptions{..} password salt argon2i argon2d postProcess =
                   64
                   out
                   512
-     case res of
-       a
-         | a `elem` [FFI.ARGON2_OK] -> postProcess out
-         | a `elem` [FFI.ARGON2_SALT_TOO_SHORT,FFI.ARGON2_SALT_TOO_LONG] ->
-           throwIO (Argon2SaltLengthOutOfRange saltLen
-                                               FFI.ARGON2_MIN_SALT_LENGTH
-                                               FFI.ARGON2_MAX_SALT_LENGTH)
-         | a `elem` [FFI.ARGON2_PWD_TOO_SHORT,FFI.ARGON2_PWD_TOO_LONG] ->
-           throwIO (Argon2PasswordLengthOutOfRange passwordLen
-                                                   FFI.ARGON2_MIN_PWD_LENGTH
-                                                   FFI.ARGON2_MAX_PWD_LENGTH)
-         | a `elem` [FFI.ARGON2_TIME_TOO_SMALL,FFI.ARGON2_TIME_TOO_LARGE] ->
-           throwIO (Argon2IterationCountOutOfRange hashIterations
-                                                   FFI.ARGON2_MIN_TIME
-                                                   FFI.ARGON2_MAX_TIME)
-         | a `elem` [FFI.ARGON2_MEMORY_TOO_LITTLE,FFI.ARGON2_MEMORY_TOO_MUCH] ->
-           throwIO (Argon2MemoryUseOutOfRange
-                      hashMemory
-                      (max FFI.ARGON2_MIN_MEMORY (8 * hashParallelism))
-                      FFI.ARGON2_MAX_MEMORY)
-         | a `elem` [FFI.ARGON2_LANES_TOO_FEW,FFI.ARGON2_LANES_TOO_MANY] ->
-           throwIO (Argon2ParallelismOutOfRange hashParallelism
-                                                FFI.ARGON2_MIN_LANES
-                                                FFI.ARGON2_MAX_LANES)
-         | otherwise -> throwIO (Argon2Exception a)
-  where argon2 = variant argon2i argon2d hashVariant
+     fmap T.decodeUtf8 $ handleSuccessCode res out saltLen passwordLen hashIterations hashMemory hashParallelism
+     where
+       argon2 = variant argon2i argon2d hashVariant
+       saltLen = fromIntegral (BS.length salt)
+       passwordLen = fromIntegral (BS.length password)
 
+hash' :: HashOptions
+      -> BS.ByteString
+      -> BS.ByteString
+      -> Argon2Raw
+      -> Argon2Raw
+      -> IO BS.ByteString
+hash' HashOptions{..} password salt argon2i argon2d =
+  do out <- mallocBytes 512
+     res <-
+       BS.useAsCString password $
+       \password' ->
+         BS.useAsCString salt $
+         \salt' ->
+           argon2 hashIterations
+                  hashMemory
+                  hashParallelism
+                  password'
+                  passwordLen
+                  salt'
+                  saltLen
+                  out
+                  512
+     handleSuccessCode res out saltLen passwordLen hashIterations hashMemory hashParallelism
+     where
+       argon2 = variant argon2i argon2d hashVariant
+       saltLen = fromIntegral (BS.length salt)
+       passwordLen = fromIntegral (BS.length password)
 
 type Argon2Verify  = CString -> CString -> Word64 -> IO Int32
 
